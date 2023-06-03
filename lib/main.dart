0ce6b293f480200f71be2acc58e821ae61b9c3f5
Shubham Daulkar <daulkarshubham@gmail.com>
2023-06-03 17:17:02 +0530

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'package:amazon_clone/constants/global_variables.dart';
+import 'package:amazon_clone/features/auth/screen/auth-screen.dart';
+import 'package:amazon_clone/router.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,18 +10,26 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Amazon Clone',
       theme: ThemeData(
+        scaffoldBackgroundColor: GlobalVariables.backgroundColor,
+        colorScheme: const ColorScheme.light(
+          primary: GlobalVariables.secondaryColor,
+        ),
+        appBarTheme: const AppBarTheme(
+            elevation: 0,
+            iconTheme: IconThemeData(
+              color: Colors.black,
+            )),
         primarySwatch: Colors.blue,
-        
-        useMaterial3: true,
       ),
-      home: const Text( 'Flutter Demo Home Page'),
+      onGenerateRoute: (settings) => generateRoute(settings),
+      home: const AuthScreen(),
+      
     );
   }
 }
-
