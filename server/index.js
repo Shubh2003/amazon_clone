4941cdf3f7c68ce417c3ec9b13e9a16e9c0241bc
Shubham Daulkar <daulkarshubham@gmail.com>
2023-06-14 17:55:03 +0530

    mongoDB setup done

--- server/index.js
+++ server/index.js
@@ -1,13 +1,27 @@
-console.log("Hello World");
-
+// IMPORTS FROM PACKAGES
 const express = require("express");
+const mongoose = require('mongoose');
 
+//IMPORTS FROM OYHER FILES
+const authRouter = require('./routes/auth');
+//INIT
 const PORT = 3000;
-
 const app = express();
+const DB = "mongodb+srv://daulkarshubham:Prathamesh2002@cluster0.xxmzznw.mongodb.net/?retryWrites=true&w=majority";
 
+app.use(authRouter);
+
+
+//COnnections
+
+mongoose.connect(DB).then(()=>{
+    console.log("Connection successful");
+})
+.catch((e)=>{
+    console.log(e);
+});
 // creating an api
-app.listen(PORT,"0.0.0.0",() => {
+app.listen(PORT,() => {
     console.log(`Connectaed at port ${PORT} hello`);
 });
 
